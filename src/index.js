d64f9d7bf28b31484d69bc592283fd9055fceb1d
Anna <61065956+AnnaIgnatova@users.noreply.github.com>
2021-02-15 23:08:51 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
 
 exports.min = function min (array) {
-  return 0;
+  try {
+    if (array.length > 0)
+      return Math.min.apply(null, array);
+    else 
+      return 0;
+  } catch(e) {
+    return 0;
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  try {
+    if (array.length > 0) 
+      return Math.max.apply(null, array);
+    else 
+      return 0;
+
+  } catch(e) {
+    return 0;
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  try {
+
+    if (array.length > 0)  {
+      let s = 0;
+
+      for (let i = 0; i < array.length; i++) {
+        s += array[i];
+      }
+
+      return s / array.length;
+    }
+    else return 0;
+    
+  } catch(e) {
+
+    return 0;
+  }
 }
